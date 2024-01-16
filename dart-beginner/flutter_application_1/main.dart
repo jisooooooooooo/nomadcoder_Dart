735fd6ab757c4a151a381d23ee1b590d2f8015e4
jisooooooooooo <dlawltn0708@naver.com>
2024-01-16 17:00:13 +0900

    lists

--- dart-beginner/flutter_application_1/main.dart
+++ dart-beginner/flutter_application_1/main.dart
@@ -1,9 +1,11 @@
 void main(){
-  String name = "jisoo";
-  bool alive = true;
-  int age = 12;
-  double money = 24.22;
-  num x = 12;
-  x = 1.1;
-
+  var giveMeFive = true;
+  var numbers = [
+  1,
+  2,
+  3,
+  4,
+  if (giveMeFive) 5,
+  ];
+  print(numbers);
 }
