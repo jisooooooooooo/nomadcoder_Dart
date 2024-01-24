3342bd1f31b7aa35072c1fa6969924c833d2a279
jisooooooooooo <dlawltn0708@naver.com>
2024-01-24 12:20:45 +0900

    QQ operator

--- dart-beginner/flutter_application_1/main.dart
+++ dart-beginner/flutter_application_1/main.dart
@@ -1,7 +1,4 @@
-String sayHello(String name, int age, [String? country = 'cuba']) =>
-    'Hello $name, you are $age years old from $country';
-
 void main() {
-  var results = sayHello('jisoo', 2);
-  print(results);
+  String? name;
+  name ??= 'jisoo';
 }
