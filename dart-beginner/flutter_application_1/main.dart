a6b486d5de6922b64e0fbf64344bf722adab20ee
jisooooooooooo <dlawltn0708@naver.com>
2024-01-27 14:21:30 +0900

    inheritance

--- dart-beginner/flutter_application_1/main.dart
+++ dart-beginner/flutter_application_1/main.dart
@@ -1,42 +1,29 @@
-abstract class Human {
-  void walk();
-}
-
-enum Team { red, blue}
-enum XPLevel {beginner, medium, pro}
-
-class Player extends Human {
-  String name;
-  XPLevel xp;
-  Team team;
-
-  Player({required this.name, required this.xp, required this.team})
-
-  void walk(){
-    print("i'm walking");
-  }
-
+class Human {
+  final String name;
+  Human({required this.name});
   void sayHello() {
     print("Hi my name is $name");
   }
 }
 
-class Coach extends Human {
-  void walk(){
-    print('the couch is walking');
+enum Team { blue, red }
+
+class Player extends Human {
+  final Team team;
+
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name);
+
+  @override 
+  void sayHello(){
+    super.sayHello();
+    print('and I play for ${team}');
   }
 }
 
 void main() {
-  var jisoo = Player(
-  name: 'jisoo', 
-  xp: XPLevel.medium, 
-  team: Team.red
-  );
-
-  var potato = jisoo
-  ..name = 'las'
-  ..xp = XPLevel.pro
-  ..team = Team.blue
-  ..sayHello();
+  var player = Player(team: Team.red, name:'jisoo', );
+  player.sayHello();
 }
